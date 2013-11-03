cb19053e94499bb11bba5ffce767aa99fcdfe944
yqhuang.msysgit <19123767@qq.com>
2013-11-03 11:55:37 +0800

    modify db script

--- database/project/db_init.sql
+++ database/project/db_init.sql
@@ -1,3 +1,12 @@
 -- --------------------------------------------------------------------
 -- 系统建表脚本 不可重复执行
 -- --------------------------------------------------------------------
+CREATE TABLE `sys_right` (
+   `id` varchar(50) character set gbk NOT NULL COMMENT '权限ID',
+   `rightname` varchar(100) character set gbk NOT NULL COMMENT '权限名称',
+   `url` varchar(255) character set gbk NOT NULL COMMENT '链接地址',
+   `rightsortno` varchar(8) character set gbk NOT NULL COMMENT '排序号',
+   `parentid` varchar(50) character set gbk NOT NULL COMMENT '上级权限主键',
+   `memo` varchar(255) character set gbk default NULL COMMENT '备注',
+   PRIMARY KEY  (`id`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
